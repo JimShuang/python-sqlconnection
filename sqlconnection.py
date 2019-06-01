6025723c27e0b9dbcdde75d236f2c9c8f049086a
Jim Shuang <42414918+JimShuang@users.noreply.github.com>
2019-05-31 23:26:09 -0400

    Update sqlconnection.py

--- sqlconnection.py
+++ sqlconnection.py
@@ -2,13 +2,16 @@ import pyodbc
 
 cnxn = pyodbc.connect(
     "DRIVER={ODBC Driver 13 for SQL Server};"
-    "Server=uspp01-24860414\eaudit;"
-    "Database=Practice;"
+    "Server=Servername;"
+    "Database=Databasename;"
     "Trusted_connection=yes;"
 )
+#Please make sure there is no space in between =, otherwise the connection will not work!
+#Please replace the Driver, Server and Database to your own ones
+
 
 cursor = cnxn.cursor()
-cursor.execute('SELECT * FROM dbo.Employee')
+cursor.execute('SELECT * FROM tablename')
 
 for row in cursor:
     print(row)
